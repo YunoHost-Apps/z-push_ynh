b5f1e06f35572c3768d27a1a1f251de938abbfbf
yalh76 <yalh@yahoo.com>
2019-03-25 23:01:02 +0100

    fix dependencies

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,6 +4,9 @@
 # COMMON VARIABLES
 #=================================================
 
+# dependencies used by the app
+pkg_dependencies="z-push-common z-push-backend-caldav z-push-backend-carddav z-push-backend-combined z-push-backend-imap z-push-backend-ldap z-push-backend-galsearch-ldap z-push-autodiscover php-memcached php-cli php-soap"
+
 #=================================================
 # PERSONAL HELPERS
 #=================================================
