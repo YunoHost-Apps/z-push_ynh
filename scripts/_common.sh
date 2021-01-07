a925c085631f458968c3978215908f9919c0f51a
yalh76 <yalh@yahoo.com>
2021-01-07 20:10:33 +0100

    Fix for stretch

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,10 +4,12 @@
 # COMMON VARIABLES
 #=================================================
 
+# dependencies used by the app
+pkg_dependencies="libawl-php"
+
 YNH_PHP_VERSION="7.3"
 
-# dependencies used by the app
-pkg_dependencies="php${YNH_PHP_VERSION}-soap php${YNH_PHP_VERSION}-imap php${YNH_PHP_VERSION}-xsl php${YNH_PHP_VERSION}-curl libawl-php php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-ldap php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-memcached"
+extra_php_dependencies="php${YNH_PHP_VERSION}-soap php${YNH_PHP_VERSION}-imap php${YNH_PHP_VERSION}-xsl php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-ldap php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-memcached"
 
 #=================================================
 # PERSONAL HELPERS
